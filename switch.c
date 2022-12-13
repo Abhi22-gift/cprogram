ba8c102fd7a1d4040bd28932385609f30b918fba
Abhi22-gift <119336084+Abhi22-gift@users.noreply.github.com>
2022-12-13 19:04:30 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter any alphabet");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case 'a':
+		printf("it is a vowel");
+		break;
+		case 'e':
+		printf("it is a vowel");
+		break;
+		case 'i':
+		printf("it is a vowel");
+		break;
+		case 'o':
+			printf("it is a vowel");
+			break;
+			case 'u':
+			printf("it is a vowel");
+			break;
+		case'A':
+				printf("it is a vowel");
+				break;
+				case 'E':
+					printf("it is a vowel");
+					break;
+					case 'I':
+						printf("it is a vowel");
+						break;
+						case '0':
+							printf("it is a vowel");
+							break;
+							case 'U':
+								printf("it is a vowel");
+								break;
+								printf("it is a consontent");
+							
+	}
+}
