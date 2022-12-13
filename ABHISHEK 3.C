cff9964897d2d79cdb55755f6464e634d8580779
Abhi22-gift <119336084+Abhi22-gift@users.noreply.github.com>
2022-12-13 19:08:53 +0530

    Add files via upload

---
+++ ABHISHEK 3.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int A;
+	printf("Enter tha numbaera A");
+scanf("%d",&A);
+if(A>0)
+{
+	printf("A positive number %d",A);
+
+}
+
+ if(A>0)
+ {
+printf("%d nagtive", A);
+
+}
+printf("%d is zero", A);
+
+}
+
+ 
